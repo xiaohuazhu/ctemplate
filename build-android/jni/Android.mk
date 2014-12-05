28adad6d338d783cc18c37598a6506e7cf34968d
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2014-12-04 17:27:19 -0800

    Cleanup of android build file

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -14,13 +14,10 @@ src_files = \
 src_dir := $(LOCAL_PATH)/../../src
 
 NDK_TOOLCHAIN_VERSION := clang
-#LOCAL_CPPFLAGS += -stdlib=libstdc++
 LOCAL_C_INCLUDES := $(src_dir)/../build-android/include $(src_dir) \
   $(ANDROID_NDK)/sources/cxx-stl/llvm-libc++/libcxx/include \
   $(ANDROID_NDK)/sources/android/support/include
 LOCAL_MODULE := ctemplate
 LOCAL_SRC_FILES := $(addprefix $(src_dir)/, $(src_files))
-#LOCAL_LDLIBS += -L$(ANDROID_NDK)/sources/cxx-stl/llvm-libc++/libs/$(TARGET_ARCH_ABI) -lc++_shared
 
-#include $(BUILD_SHARED_LIBRARY)
 include $(BUILD_STATIC_LIBRARY)
